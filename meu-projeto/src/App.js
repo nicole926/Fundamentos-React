4ed89f119f620edeca7fb306d625dc68337a62fc
nicole926 <nicolemendes.dev@gmail.com>
2026-09-15 14:17:13 -0300

    commit

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, mundo!</h1>
+      <p>Este é o meu primeiro aplicativo React.</p>
+    </div>
+  );
+}
+
+export default App;
